e1e8599e595bf7e26cce63505b9da6d8f44386b4
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-28 19:52:36 +0400

    Initial Commit
    Screens configuration
    Navigation functionality configuration
    React Native Elements installed
    other necessary dependencies and imports installed

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const AccountScreen = () => {
+    return <View>
+        <Text style={{ fontSize: 48 }}>AccounScreen</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({});
+
+export default AccountScreen;
