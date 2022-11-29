55098af145c0150d27aa2eb30d58cde0c6e7918c
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-29 12:11:50 +0400

    useState variables & input added to signup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
     return (
         <View style={styles.container}>
             <Spacer>
                 <Text h3>Sign up for Tracker</Text>
             </Spacer>
-            <Input label="Email" />
+            <Input
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
             <Spacer />
-            <Input label="Password" />
+            <Input
+                secureTextEntry
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
             <Spacer>
                 <Button title="Sign Up" />
             </Spacer>
