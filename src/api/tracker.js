207ad575dc7731525b8e09518b3700ecaf5d2376
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-29 14:24:33 +0400

    baseUrl added

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://cae6-45-9-46-146.ngrok.io'
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
     switch (action.type) {
@@ -6,8 +7,35 @@ const authReducer = (state, action) => {
     }
 };
 
+const signup = dispatch => {
+    return async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signup', { email, password });
+            console.log(response.data);
+        } catch (err) {
+            console.log(err.message);
+        }
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // make api request to sign in with email and password
+
+        // if we sign in, modify our state and set to authenticated
+
+        // if signing in fails show error message
+    };
+};
+
+const signout = (dispatch) => {
+    return ({ email, password }) => {
+
+    };
+};
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    {  signin, signout, signup },
     { isSignedIn: false }
 );
