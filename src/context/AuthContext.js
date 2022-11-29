70b2df495fbd664b888feb1016f8062614bfae2f
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-29 14:50:51 +0400

    signup error handling

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,6 +3,8 @@ import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload };  
         default: state;
     }
 };
@@ -13,7 +15,7 @@ const signup = dispatch => {
             const response = await trackerApi.post('/signup', { email, password });
             console.log(response.data);
         } catch (err) {
-            console.log(err.message);
+            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
         }
     };
 };
@@ -36,6 +38,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {  signin, signout, signup },
-    { isSignedIn: false }
+    { signin, signout, signup },
+    { isSignedIn: false, errorMessage: '' }
 );
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -30,6 +30,9 @@ const SignupScreen = ({ navigation }) => {
                 autoCorrect={false}
                 autoCapitalize="none"
             />
+            {state.errorMessage ? (
+                <Text style={styles.errorMessage}>{state.errorMessage}</Text>)
+                : null}
             <Spacer>
                 <Button
                     title="Sign Up"
@@ -50,6 +53,12 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         marginBottom: 250
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: 'red',
+        marginLeft: 15,
+        marginTop: 15
     }
 });
 
