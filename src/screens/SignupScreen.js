2f97afd9531cf6cd4b324181a6442f287da518e6
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-29 17:18:35 +0400

    added reusable components for signup and signin

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,43 +1,25 @@
-import React, { useState, useContext } from "react";
-import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import Spacer from "../components/Spacer";
-import { Context as AuthContext } from '../context/AuthContext'
+import React, { useContext } from "react";
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from "../components/NavLink";
 
 const SignupScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+
 
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign up for Tracker</Text>
-            </Spacer>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCorrect={false}
-                autoCapitalize="none"
+            <AuthForm
+                headerText="Sign Up For Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signup}
             />
-            <Spacer />
-            <Input
-                secureTextEntry
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCorrect={false}
-                autoCapitalize="none"
+            <NavLink
+                routeName="Signin"
+                text="Already have an account? Sign in instead!"
             />
-            {state.errorMessage ? (
-                <Text style={styles.errorMessage}>{state.errorMessage}</Text>)
-                : null}
-            <Spacer>
-                <Button
-                    title="Sign Up"
-                    onPress={() => signup({ email, password })} />
-            </Spacer>
         </View>
     );
 };
@@ -53,12 +35,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         marginBottom: 250
-    },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15
     }
 });
 
